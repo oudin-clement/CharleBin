98d8cf0a6dcfe940bcaf23c8f5cbe492abc8b727
oudin-clement <66120513+oudin-clement@users.noreply.github.com>
2023-01-30 10:51:36 +0100

    Update Configuration.php

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -36,7 +36,7 @@ class Configuration
      */
     private static $_defaults = array(
         'main' => array(
-            'name'                     => 'PrivateBin',
+            'name'                     => 'CharlBin',
             'basepath'                 => '',
             'discussion'               => true,
             'opendiscussion'           => false,
